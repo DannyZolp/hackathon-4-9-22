dbbf3a460228bd8f32aee3c699aada9b0de9e8a2
Danny Zolp <danny@zolp.dev>
2022-04-09 12:59:49 -0500

    fetch basic weather data

--- hackathon-4-9-22/Form1.cs
+++ hackathon-4-9-22/Form1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Flurl;
+using Flurl.Http;
+using Newtonsoft.Json;
 
 namespace hackathon_4_9_22
 {
@@ -15,6 +20,25 @@ namespace hackathon_4_9_22
         public Form1()
         {
             InitializeComponent();
+            grabDataAsync(43.221600, -88.014832);
+        }
+
+        public async Task grabDataAsync(double latitude, double longitude)
+        {
+            try
+            {
+                using (var fc = new FlurlClient().WithHeader("User-Agent", "(dannyzolp.com, danny@zolp.dev)").WithHeader("Accept", "application/geo+json"))
+                {
+                    var userModel = await string.Format("https://api.weather.gov/points/{0},{1}", latitude, longitude)
+                        .WithClient(fc)
+                        .GetJsonAsync();
+
+                    Console.WriteLine(JsonConvert.SerializeObject(userModel));
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
