02f0aa14323380c20565dd291a9aca487d2e7c7d
Danny Zolp <danny@zolp.dev>
2022-04-09 14:57:22 -0500

    image fix

--- hackathon-4-9-22/Form1.Designer.cs
+++ hackathon-4-9-22/Form1.Designer.cs
@@ -34,14 +34,16 @@ namespace hackathon_4_9_22
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(178, 101);
+            this.textBox1.Location = new System.Drawing.Point(237, 124);
             this.textBox1.Margin = new System.Windows.Forms.Padding(1);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.Size = new System.Drawing.Size(167, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Enter City & State";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -49,10 +51,10 @@ namespace hackathon_4_9_22
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(191, 134);
+            this.button1.Location = new System.Drawing.Point(255, 165);
             this.button1.Margin = new System.Windows.Forms.Padding(1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 44);
+            this.button1.Size = new System.Drawing.Size(133, 54);
             this.button1.TabIndex = 1;
             this.button1.Text = "Get Weather";
             this.button1.UseVisualStyleBackColor = true;
@@ -61,34 +63,44 @@ namespace hackathon_4_9_22
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(206, 73);
+            this.label1.Location = new System.Drawing.Point(275, 90);
             this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(91, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Weather App";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(183, 212);
+            this.textBox2.Location = new System.Drawing.Point(244, 261);
             this.textBox2.Margin = new System.Windows.Forms.Padding(1);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(116, 20);
+            this.textBox2.Size = new System.Drawing.Size(153, 22);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(183, 245);
+            this.textBox3.Location = new System.Drawing.Point(244, 302);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(116, 20);
+            this.textBox3.Size = new System.Drawing.Size(153, 22);
             this.textBox3.TabIndex = 4;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(498, 168);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 339);
+            this.ClientSize = new System.Drawing.Size(643, 417);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
@@ -97,6 +109,7 @@ namespace hackathon_4_9_22
             this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "Form1";
             this.Text = "Weather Application";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,6 +122,7 @@ namespace hackathon_4_9_22
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
