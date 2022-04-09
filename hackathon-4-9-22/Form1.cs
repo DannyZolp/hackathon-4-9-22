ec590817f6231042744587d7ba654a046a09b5ef
Danny Zolp <danny@zolp.dev>
2022-04-09 13:21:19 -0500

    added in temperature fetching

--- hackathon-4-9-22/Form1.cs
+++ hackathon-4-9-22/Form1.cs
@@ -11,29 +11,40 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Flurl;
 using Flurl.Http;
+using hackathon_4_9_22.JsonFormats;
 using Newtonsoft.Json;
 
 namespace hackathon_4_9_22
 {
     public partial class Form1 : Form
     {
+        public int temperature;
+
         public Form1()
         {
             InitializeComponent();
-            grabDataAsync(43.221600, -88.014832);
+            grabWeatherDataAsync(43.221600, -88.014832);
         }
 
-        public async Task grabDataAsync(double latitude, double longitude)
+        public async Task grabWeatherDataAsync(double latitude, double longitude)
         {
             try
             {
                 using (var fc = new FlurlClient().WithHeader("User-Agent", "(dannyzolp.com, danny@zolp.dev)").WithHeader("Accept", "application/geo+json"))
                 {
-                    var userModel = await string.Format("https://api.weather.gov/points/{0},{1}", latitude, longitude)
+                    var pointsReq = await string.Format("https://api.weather.gov/points/{0},{1}", latitude, longitude)
                         .WithClient(fc)
-                        .GetJsonAsync();
+                        .GetJsonAsync<PointsRequest>();
 
-                    Console.WriteLine(JsonConvert.SerializeObject(userModel));
+                    //Console.WriteLine("Request finished");
+                    //Console.WriteLine(pointsReq);
+                    //Console.WriteLine(pointsReq.properties.forecast);
+
+                    var forecastReq = await pointsReq.properties.forecast.WithClient(fc).GetJsonAsync<ForecastRequest>();
+
+                    this.temperature = forecastReq.properties.periods[0].temperature;
+
+                    Console.WriteLine(string.Format("It is {0}{1}", forecastReq.properties.periods[0].temperature, forecastReq.properties.periods[0].temperatureUnit));
                 }
             } catch (Exception e)
             {
---
+++ hackathon-4-9-22/JsonFormats/ForecastRequest.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackathon_4_9_22.JsonFormats
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class ForecastGeometry
+    {
+        public string type { get; set; }
+        public List<List<List<double>>> coordinates { get; set; }
+    }
+
+    public class Elevation
+    {
+        public string unitCode { get; set; }
+        public double value { get; set; }
+    }
+
+    public class Period
+    {
+        public int number { get; set; }
+        public string name { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public bool isDaytime { get; set; }
+        public int temperature { get; set; }
+        public string temperatureUnit { get; set; }
+        public string temperatureTrend { get; set; }
+        public string windSpeed { get; set; }
+        public string windDirection { get; set; }
+        public string icon { get; set; }
+        public string shortForecast { get; set; }
+        public string detailedForecast { get; set; }
+    }
+
+    public class ForecastProperties
+    {
+        public DateTime updated { get; set; }
+        public string units { get; set; }
+        public string forecastGenerator { get; set; }
+        public DateTime generatedAt { get; set; }
+        public DateTime updateTime { get; set; }
+        public string validTimes { get; set; }
+        public Elevation elevation { get; set; }
+        public List<Period> periods { get; set; }
+    }
+
+    public class ForecastRequest
+    {
+        [JsonProperty("@context")]
+        public List<object> Context { get; set; }
+        public string type { get; set; }
+        public ForecastGeometry geometry { get; set; }
+        public ForecastProperties properties { get; set; }
+    }
+
+
+}
