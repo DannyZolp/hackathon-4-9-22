70a63d3c81cb85d269dc2ad9b57b8af538a6cb2a
Lucas Berth <lucas.berth@cuw.edu>
2022-04-09 16:49:09 -0500

    finalizing format changes

--- hackathon-4-9-22/Form1.Designer.cs
+++ hackathon-4-9-22/Form1.Designer.cs
@@ -44,10 +44,10 @@ namespace hackathon_4_9_22
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(475, 241);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(178, 56);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(329, 38);
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Enter City & State";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -56,10 +56,10 @@ namespace hackathon_4_9_22
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Silver;
-            this.button1.Location = new System.Drawing.Point(509, 320);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(191, 89);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(267, 105);
+            this.button1.Size = new System.Drawing.Size(100, 44);
             this.button1.TabIndex = 1;
             this.button1.Text = "Get Weather";
             this.button1.UseVisualStyleBackColor = false;
@@ -67,27 +67,26 @@ namespace hackathon_4_9_22
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(475, 631);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Location = new System.Drawing.Point(183, 238);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(303, 38);
+            this.textBox2.Size = new System.Drawing.Size(116, 20);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(475, 704);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.textBox3.Location = new System.Drawing.Point(183, 262);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(303, 38);
+            this.textBox3.Size = new System.Drawing.Size(116, 20);
             this.textBox3.TabIndex = 4;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(107, 305);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.pictureBox1.Location = new System.Drawing.Point(40, 128);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(219, 196);
+            this.pictureBox1.Size = new System.Drawing.Size(82, 82);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
@@ -95,27 +94,27 @@ namespace hackathon_4_9_22
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.ImageLocation = "";
-            this.pictureBox2.Location = new System.Drawing.Point(0, 2);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.pictureBox2.Location = new System.Drawing.Point(0, 1);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(1288, 811);
+            this.pictureBox2.Size = new System.Drawing.Size(483, 340);
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(371, 457);
+            this.textBox4.Location = new System.Drawing.Point(144, 147);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(513, 120);
+            this.textBox4.Size = new System.Drawing.Size(195, 78);
             this.textBox4.TabIndex = 7;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1285, 808);
+            this.ClientSize = new System.Drawing.Size(482, 339);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox3);
